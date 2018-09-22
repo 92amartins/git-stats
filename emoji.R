dfbfa62e3e8664a0541ab4ce800fa7462bc5a830
Andrei Martins <amartins13@outlook.com>
2018-09-22 20:01:19 -0300

    :sparkles: Get emoji directly from repo

--- emoji.R
+++ emoji.R
@@ -2,9 +2,11 @@ library(readtext)
 library(tidytext)
 library(tidyverse)
 library(ggthemes)
+library(jsonlite)
 
-commits <- readtext("data/stewie_commits.txt")
-emojis <- read.csv("data/emojis.csv", stringsAsFactors = FALSE)
+commits <- readtext("data/stewie-commits.txt")
+emojis <- read_json("https://raw.githubusercontent.com/carloscuesta/gitmoji/master/src/data/gitmojis.json",
+                    simplifyVector = TRUE)$gitmojis
 
 
 tidy_commits <- commits %>%
@@ -13,15 +15,15 @@ tidy_commits <- commits %>%
   select(word)
 
 emoji_counts <- tidy_commits %>%
-  inner_join(emojis, by = c("word" = "name")) %>%
-  count(word, sort = TRUE) %>%
+  inner_join(emojis, by = c("word" = "code")) %>%
+  count(emoji, sort = TRUE) %>%
   mutate(percent = paste0(round((n / sum(n)) * 100, 2), "%"))
 
 emoji_counts %>%
-  mutate(word = reorder(word, n)) %>%
+  mutate(emoji = reorder(emoji, n)) %>%
   ggplot() +
-  geom_text(aes(word,n, label = percent)) +
-  geom_text(aes(word,n, label = word), nudge_y = 5) +
+  geom_text(aes(emoji,n, label = percent)) +
+  geom_text(aes(emoji,n, label = emoji), nudge_y = 8) +
   labs(x="Emoji", y="# commits",
        title = "Distribuição de commits no Stewie",
        subtitle = "Carga de bugs de 23%",
